9b1a4201887b4fd891db54bd6c49beb250fa66c7
ThanhPMSE63054 <ThanhPMSE63054@fpt.edu.vn>
2019-10-03 12:22:53 +0700

    version2

--- xavalocoffee/src/main/java/group3/xavalocoffee/controller/ManagerController.java
+++ xavalocoffee/src/main/java/group3/xavalocoffee/controller/ManagerController.java
@@ -1,6 +1,5 @@
 package group3.xavalocoffee.controller;
 
-
 import group3.xavalocoffee.constant.Constant;
 import group3.xavalocoffee.dto.BillResponseDTO;
 
@@ -25,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping(Constant.API)
 public class ManagerController {
@@ -36,133 +34,133 @@ public class ManagerController {
     ManagerService managerService;
 
     @GetMapping(Constant.MANAGER_API + "/{tableNumber}")
-    public ResponseEntity<ServiceResponseDTO> getInfoTable(@PathVariable int tableNumber){
+    public ResponseEntity<ServiceResponseDTO> getInfoTable(@PathVariable int tableNumber) {
         logger.info(Constant.BEGIN + "getInfoTable");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             BillResponseDTO dto = managerService.getInfoOfBill(tableNumber);
             response.setData(dto);
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "getInfoTable");
         }
     }
 
     @PostMapping(Constant.MANAGER_API + "/addDrinkForTable")
-    public ResponseEntity addDrinkForTable(@RequestBody DrinkOrderRequestDTO requestDTO){
+    public ResponseEntity addDrinkForTable(@RequestBody DrinkOrderRequestDTO requestDTO) {
         logger.info(Constant.BEGIN + "addDrinkForTable");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             managerService.addDrinkForTable(requestDTO);
             response.setMessage("Added drink for table");
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "addDrinkForTable");
         }
     }
 
     @PutMapping(Constant.MANAGER_API + "/addCount/{billInfoId}")
-    public ResponseEntity addCount(@PathVariable int billInfoId){
+    public ResponseEntity addCount(@PathVariable int billInfoId) {
         logger.info(Constant.BEGIN + "addCount");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             managerService.addCountDrink(billInfoId);
             response.setMessage("Add count for drink");
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "addCount");
         }
     }
 
     @PutMapping(Constant.MANAGER_API + "/subCount/{billInfoId}")
-    public ResponseEntity subCount(@PathVariable int billInfoId){
+    public ResponseEntity subCount(@PathVariable int billInfoId) {
         logger.info(Constant.BEGIN + "subCount");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             managerService.subCountDrink(billInfoId);
             response.setMessage("Sub count for drink");
-            return  new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+            return new ResponseEntity(response, HttpStatus.OK);
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "subCount");
         }
     }
 
     @PutMapping(Constant.MANAGER_API + "/switchTables/{tableNumberFrom}/{tableNumberTo}")
-    public ResponseEntity switchTables(int tableNumberFrom, int tableNumberTo){
+    public ResponseEntity switchTables(int tableNumberFrom, int tableNumberTo) {
         logger.info(Constant.BEGIN + "switchTables");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             managerService.switchTables(tableNumberFrom, tableNumberTo);
             response.setMessage("Switched table");
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "switchTables");
         }
     }
 
     @DeleteMapping(Constant.MANAGER_API + "/removeDrink/{billInfoId}")
-    public ResponseEntity removeDrink(@PathVariable int billInfoId){
+    public ResponseEntity removeDrink(@PathVariable int billInfoId) {
         logger.info(Constant.BEGIN + "removeDrink");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             managerService.removeDrink(billInfoId);
             response.setMessage("Removed drink from table");
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "removeDrink");
         }
     }
 
     @PutMapping(Constant.MANAGER_API + "/addDiscount/{billId}/{discount}")
-    public ResponseEntity addDiscount(@PathVariable int billId, @PathVariable float discount){
+    public ResponseEntity addDiscount(@PathVariable int billId, @PathVariable float discount) {
         logger.info(Constant.BEGIN + "addDiscount");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             managerService.addDiscount(billId, discount);
             response.setMessage("Added discount for bill");
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "addDiscount");
         }
     }
 
     @PutMapping(Constant.MANAGER_API + "/payment/{billId}")
-    public ResponseEntity payment(@RequestHeader String Authorization, @PathVariable int billId){
+    public ResponseEntity payment(@RequestHeader String Authorization, @PathVariable int billId) {
         logger.info(Constant.BEGIN + "payment");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
@@ -171,32 +169,33 @@ public class ManagerController {
             managerService.payment(billId, username);
             response.setMessage("Checked out");
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "payment");
         }
     }
 
     @GetMapping(Constant.MANAGER_API + "/getbill")
-    public ResponseEntity statisticByUsername(@RequestHeader String Authorization){
+    public ResponseEntity statisticByUsername(@RequestHeader String Authorization) {
         logger.info(Constant.BEGIN + "statisticByUsername");
         ServiceResponseDTO response = new ServiceResponseDTO();
         try {
             String token = Authorization.substring(7);
             String username = JWTVerifier.getDecodedJWT(token).getSubject();
             List<Bill> billList = managerService.getBillByUsername(username);
+
             response.setData(billList);
             return new ResponseEntity(response, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex);
             response.setMessage(ex.getMessage());
             response.setStatus(ServiceResponseDTO.Status.FAILED);
-            return new ResponseEntity(response,HttpStatus.FAILED_DEPENDENCY);
-        }finally {
+            return new ResponseEntity(response, HttpStatus.FAILED_DEPENDENCY);
+        } finally {
             logger.info(Constant.END + "statisticByUsername");
         }
     }
